d36c26964ec7bcd81ad66f63896c72c5152d027c
hoshina <hoshina12345@gmail.com>
2019-09-03 20:02:06 +0900

    progress program

--- src/step_costmap.cpp
+++ src/step_costmap.cpp
@@ -1,8 +1,56 @@
 #include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <geometry_msgs/Point32.h>
+#include <tf/transform_listener.h>
+#include <costmap_2d/costmap_2d.h>
+
+class StepCostmap
+{
+public:
+	StepCostmap();
+private:
+	ros::NodeHandle nh_;
+	ros::Subscriber cloud_sub_;
+	ros::Publisher cloud_pub_;
+	tf::TransformListener tf_listener_;
+	void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msgs);
+	costmap_2d::Costmap2D step_costmap_;
+
+	std::string sensor_frame_, topic_name_;
+};
+
+StepCostmap::StepCostmap()
+{
+	std::cout << "start Step Costmap class" << std::endl;
+	
+	ros::NodeHandle private_nh("~");
+	private_nh.param("sensor_frame", sensor_frame_, std::string("/velodyne"));
+	private_nh.param("topic_name", topic_name_, std::string("velodyne_points"));
+	
+	step_costmap_.setDefaultValue(0);
+	step_costmap_.resizeMap(40, 40, 0.1, 0, 0);
+	
+	cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/step_cloud", 1, false);
+	cloud_sub_ = nh_.subscribe(topic_name_, 1, &StepCostmap::cloudCallback, this);
+}
+
+void StepCostmap::cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msgs)
+{
+	std::cout << "cloud callback" << std::endl;
+}
+
 
 
 int main(int argc, char **argv)
 {
     std::cout << "start step_costmap" << std::endl;
-    return 0;
+	ros::init(argc, argv, "step_costmap");
+	StepCostmap sc;
+	ros::Rate r(5);
+	while(ros::ok()){
+		ros::spinOnce();
+		r.sleep();
+	}
+	return 0;
 }
